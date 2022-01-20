c57e1b9512be586dc00645c8e526b1b4a15b3a5e
Aaron.Li <Free_Aaron.Li@outlook.com>
2022-01-20 23:08:57 +0800

    更改Two_Sum

--- leetcode/src/s0001/Two_Sum.java
+++ leetcode/src/s0001/Two_Sum.java
@@ -13,14 +13,14 @@ public class Two_Sum {
      * @return 返回数组下标的位置，如果没有找到则返回null
      * 找到返回例："[2,3]"
      * 未找到返回例： return null
-     */                                                                   //这是我的源代码
+     */
     public static String two_Sum(int[] numbers, int target) {
         int a = numbers.length;
         for (int i = 0; i < a - 1; i++) {
             for (int j = i + 1; j < a; j++) {
                 if (numbers[i] + numbers[j] == target) {
                     return "[" + i + "," + j + "]";
-                }//寻找最优解；
+                }
             }
         }
         return null;
