e619e60821282815c08d327bda014bdce5e02d9e
Aaron.Li <Free_Aaron.Li@outlook.com>
2022-01-22 23:16:28 +0800

    更新Test36_TransferTest,新增例题：求圆面积

--- Txet/src/top/kukuli/Test36_TransferTest.java
+++ Txet/src/top/kukuli/Test36_TransferTest.java
@@ -9,7 +9,49 @@ package top.kukuli;
 
 /**
  * 关于方法的参数传递的一些例题；
+ * <p>
+ * 例题一：值传递考察；
+ * <p>
+ * 例题一：值传递考察；
+ * <p>
+ * 例题一：值传递考察；
+ * <p>
+ * 例题一：值传递考察；
+ * <p>
+ * 例题一：值传递考察；
+ * <p>
+ * 例题一：值传递考察；
+ * <p>
+ * 例题一：值传递考察；
+ * <p>
+ * 例题一：值传递考察；
+ * <p>
+ * 例题一：值传递考察；
+ * <p>
+ * 例题一：值传递考察；
+ * <p>
+ * 例题一：值传递考察；
+ * <p>
+ * 例题一：值传递考察；
+ * <p>
+ * 例题一：值传递考察；
+ * <p>
+ * 例题一：值传递考察；
+ * <p>
+ * 例题一：值传递考察；
+ * <p>
+ * 例题一：值传递考察；
+ * <p>
+ * 例题一：值传递考察；
+ * <p>
+ * 例题一：值传递考察；
+ * <p>
+ * 例题一：值传递考察；
  */
+/**
+ * 例题一：值传递考察；
+ */
+/*
 public class Test36_TransferTest {
     public static void main(String[] args) {
         Test36_TransferTest test = new Test36_TransferTest();
@@ -35,4 +77,52 @@ public class Test36_TransferTest {
 
 class Value1 {
     int i = 15;
+    }
+*/
+
+/**
+ * 例题二：
+ * （1） 定义一个Circle类，包含一个double型的radius属性代表圆的半径，一个findArea()方法返回圆的面积；
+ * （2） 定义一个PassObject类，在类中定义一个方法printAreas()，该方法的定义如下：
+ *  public void printAreas(Circle c,int time)。在printAreas方法中打印输出1到time之间的每个整数半径值，
+ *  以及对应的面积。
+ *  例如：times为5，则输出半径1，2，3，4，5，以及对应的圆面积。
+ */
+public class Test36_TransferTest {
+    public static void main(String[] args) {
+        System.out.println("Radius             " + "Area");
+        PassObject test = new PassObject();
+        Circle a = new Circle();
+        test.printAreas(a, 5);
+    }
+}
+
+class Circle {
+    /**
+     * 圆半径
+     */
+    double radius;
+
+    /**
+     * 计算圆面积；
+     * @param radius：半径
+     * @return area 圆面积；
+     */
+    public double findArea(double radius) {
+        double area;
+        area = 3.1415926 * radius * radius;
+        return area;
+    }
+}
+
+class PassObject {
+    public void printAreas(Circle c, int time) {
+        Circle p = new Circle();
+        for (double i = 1; i <= time; i++) {
+            c.radius = i;
+            System.out.println(i + "                  " + p.findArea(c.radius));
+        }
+        System.out.println();
+        System.out.println("now radius is:" + time);
+    }
 }
